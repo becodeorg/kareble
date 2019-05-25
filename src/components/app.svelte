0176e9d555eadb4f0738471a37503575b4638855
leny <info@flatland.be>
2019-05-26 00:45:03 +0200

    Intro text

--- src/components/app.svelte
+++ src/components/app.svelte
@@ -1,5 +1,36 @@
 <script>
     import ContributionsBoard from "./contributions/board.svelte";
+    import {totalMembers} from "../../data/contributions.json";
 </script>
+
+<h1>
+    <strong>Last year</strong>
+    <em>...on GitHub</em>
+</h1>
+
+<p>
+    Here at
+    <a href="https://becode.org">BeCode</a>
+    , we like
+    <strong>git</strong>
+    and
+    <strong>GitHub</strong>
+    .
+</p>
+
+<p>
+    All of our {totalMembers}
+    <em>BeCodians</em>
+    , coaches or learners, are using GitHub on daily basis, collecting green
+    squares on their profiles.
+</p>
+
+<p>
+    And now, introducing... our
+    <em>blue squares</em>
+    !
+    <br />
+    All the contributions of the last year, from all the BeCodians!
+</p>
 
 <ContributionsBoard />
