6c8f0edbd97fa6b04449bf1bb5bb1c17a3aa19fe
leny <info@flatland.be>
2019-05-25 18:33:03 +0200

    Add color computation in fetch data script

--- scripts/fetch-data.js
+++ scripts/fetch-data.js
@@ -19,8 +19,10 @@ const spinner = ora();
 
 const [, , token] = process.argv;
 
+const colors = ["#00b0d1", "#56c4d8", "#81cedb", "#a0d8dd", "#dddddd"];
+
 if (!token) {
-    console.log("💣", chalk.red("error:"), error.message);
+    console.log("💣", chalk.red("error:"), "No GitHub token given!");
     process.exit(1);
 }
 
@@ -82,66 +84,140 @@ const fetchBatch = async (cursor = null) => {
 };
 
 (async () => {
-    let members,
-        contributions = {};
+    let members;
+
+    const contributions = {};
 
     try {
         members = await fetchBatch();
+
+        spinner.start("Parsing data");
+
+        members.forEach(
+            ({
+                login,
+                contributionsCollection: {
+                    hasAnyContributions,
+                    contributionCalendar: {weeks},
+                },
+            }) => {
+                if (!hasAnyContributions) {
+                    return;
+                }
+
+                weeks.forEach(({contributionDays}) => {
+                    contributionDays.forEach(
+                        ({date, weekday, contributionCount}) => {
+                            if (!contributionCount) {
+                                return;
+                            }
+
+                            const [key] = date.split("T");
+
+                            if (!contributions[key]) {
+                                contributions[key] = {
+                                    date,
+                                    weekday,
+                                    members: [],
+                                };
+                            }
+
+                            contributions[key].members.push([
+                                contributionCount,
+                                login,
+                            ]);
+                        },
+                    );
+                });
+            },
+        );
+
+        const values = Object.values(contributions);
+
+        values.sort((a, b) => {
+            if (a.date < b.date) {
+                return -1;
+            }
+            if (a.date > b.date) {
+                return 1;
+            }
+            return 0;
+        });
+
+        let weeks = [];
+
+        let currentWeek = [],
+            grandTotalContributions = 0;
+
+        const contributionsCounts = new Set();
+
+        values.forEach(day => {
+            grandTotalContributions += day.totalContributions = day.members.reduce(
+                (acc, [count]) => acc + count,
+                0,
+            );
+            contributionsCounts.add(day.totalContributions);
+            currentWeek.push(day);
+            if (day.weekday === 6) {
+                weeks.push(currentWeek);
+                currentWeek = [];
+            }
+        });
+
+        spinner.succeed();
+
+        spinner.start("Compute colors");
+
+        const counts = Array.from(contributionsCounts);
+        const min = Math.min(...counts);
+        const max = Math.max(...counts);
+        const delta = max - min;
+        const tresholds = [
+            max,
+            Math.round(delta * 0.75),
+            Math.round(delta * 0.5),
+            Math.round(delta * 0.25),
+            min,
+        ];
+
+        weeks = weeks.map(week =>
+            week.map(day => {
+                day.color =
+                    colors[
+                        /* eslint-disable-next-line arrow-body-style */
+                        tresholds.reduce((cur, val, ind) => {
+                            return val >= day.totalContributions ? ind : cur;
+                        }, 0)
+                    ];
+                return day;
+            }),
+        );
+
+        spinner.succeed();
+
+        spinner.start("Writing file");
+
+        const targetFilePath = "./data/contributions.json";
+
+        await writeFile(
+            targetFilePath,
+            JSON.stringify(
+                {
+                    buildDate: Date.now(),
+                    totalContributions: grandTotalContributions,
+                    weeks,
+                },
+                null,
+                2,
+            ),
+            "utf8",
+        );
+
+        spinner.succeed(
+            `Contributions fetched and stored in ${chalk.cyan(targetFilePath)}`,
+        );
     } catch (error) {
         spinner.fail();
         console.log("💣", chalk.red("error:"), error.message);
     }
-
-    members.forEach(
-        ({
-            login,
-            contributionsCollection: {
-                hasAnyContributions,
-                contributionCalendar: {weeks},
-            },
-        }) => {
-            if (!hasAnyContributions) {
-                return;
-            }
-
-            weeks.forEach(({contributionDays}) => {
-                contributionDays.forEach(
-                    ({date, weekday, contributionCount}) => {
-                        if (!contributionCount) {
-                            return;
-                        }
-
-                        const [key] = date.split("T");
-
-                        if (!contributions[key]) {
-                            contributions[key] = {date, weekday, members: []};
-                        }
-
-                        contributions[key].members.push([
-                            contributionCount,
-                            login,
-                        ]);
-                    },
-                );
-            });
-        },
-    );
-
-    const values = Object.values(contributions);
-
-    values.sort((a, b) => {
-        if (a.date < b.date) {
-            return -1;
-        }
-        if (a.date > b.date) {
-            return 1;
-        }
-        return 0;
-    });
-
-    await writeFile(
-        "./data/contributions.json",
-        JSON.stringify(values, null, 2),
-        "utf8",
-    );
 })();
