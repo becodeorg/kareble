9114e69655954c067708cdbd07576f0ad8cdfa65
leny <info@flatland.be>
2019-05-25 23:52:44 +0200

    Basic markup/logic for contributions board

--- src/components/contributions/board.svelte
+++ src/components/contributions/board.svelte
@@ -1,9 +1,12 @@
 <script>
     import {
         buildDate,
+        totalMembers,
         totalContributions,
         weeks,
     } from "../../../data/contributions.json";
+
+    let selectedDay = null;
 </script>
 
 <style>
@@ -23,26 +26,82 @@
 
     .day {
         display: block;
-        width: 15px;
-        height: 15px;
-        margin: 2px;
+        width: 10px;
+        height: 10px;
+        border: 1px solid white;
+        cursor: pointer;
+        transition: opacity 0.25s ease-in-out;
+    }
+
+    .board.with-selection .day {
+        opacity: 0.66;
+    }
+
+    .board.with-selection .day.selected {
+        opacity: 1;
+        outline: 2px solid #15232e;
+    }
+
+    .day:hover {
+        opacity: 1;
+    }
+
+    .day.highest {
+        background: #243a5b;
+    }
+
+    .day.high {
+        background: #50a7b2;
+    }
+
+    .day.mid {
+        background: #58c4d8;
+    }
+
+    .day.low {
+        background: #a1d8dd;
+    }
+
+    .day.lowest {
+        background: #eeeeee;
     }
 </style>
 
 <div class="wrapper">
-    <h3>{totalContributions} contributions</h3>
+    <h3>{totalContributions} contributions by {totalMembers} BeCodians</h3>
 
-    <div class="board">
+    <div class="board {selectedDay ? 'with-selection' : ''}">
         {#each weeks as week}
             <div class="week">
                 {#each week as day}
                     <div
-                        class="day"
-                        style="background-color: {day.color}"
+                        on:click={() => {
+                            selectedDay = selectedDay && selectedDay.date === day.date ? null : day;
+                        }}
+                        class="day {day.color}
+                        {selectedDay && selectedDay.date === day.date ? 'selected' : ''}"
                         title="{day.date}: {day.totalContributions}
                         contributions" />
                 {/each}
             </div>
         {/each}
     </div>
+
+    {#if selectedDay && selectedDay.date}
+        <div class="contributors">
+            <h4>
+                 {selectedDay.date}: {selectedDay.totalContributions}
+                contributions by {selectedDay.members.length} BeCodians
+            </h4>
+
+            <ul>
+                {#each selectedDay.members as [count, login]}
+                    <li>
+                        <a href="https://github.com/{login}">{login}</a>
+                        ({count} contributions)
+                    </li>
+                {/each}
+            </ul>
+        </div>
+    {/if}
 </div>
